2420c087dc6f64940bd002e016dbb0dbf137857b
Nilesh-Sah <sahnilesh06@gmail.com>
2025-01-17 15:14:01 +0200

    trying to integrate both frontend and backend via API

--- myproj/urls.py
+++ myproj/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('home/', views.home , name='home'),
     path('article/<int:article_id>/', views.article_detail, name='article_detail'),
     path('create/', views.create_article , name='create_article'),
+    path('api/articles/', views.get_articles , name='get_articles'),
+    path('api/articles/create/', views.create_article, name='create_article'),
     
 ]
--- myproj/views.py
+++ myproj/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render , get_object_or_404 , redirect
 from django.http import HttpResponse
 from .models import Article
 from .forms import ArticleForm
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Article
+from .serializers import ArticleSerializers
 # Create your views here.
 
 def home(request):
@@ -28,3 +32,22 @@ def create_article(request):
 def article_list(request):
     articles=Article.objects.order_by('-published_date')
     return render(request, 'myproj/index.html', {'articles':articles})
+
+@api_view(['GET'])
+def get_articles(request):
+    articles= Article.objects.all()
+    serializer=ArticleSerializers(articles, many=True)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def create_article(request):
+    serializer=ArticleSerializers(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=201)
+    return Response(serializer.errors, status=400)
+
+
+
+
+
